408f293635f3ebd2915601b7a8bbfb67676ffc51
EARA-dean <dean@earadesigns.co.za>
2014-05-05 16:42:16 +0200

    more updates

--- includes/css.php
+++ includes/css.php
@@ -3,5 +3,18 @@
 	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
 	<link href="css/style.css" rel="stylesheet">
+
+	<link href="includes/content/top.headers/topheader1.css" rel="stylesheet">
+
 	<link href="includes/content/headers/header1.css" rel="stylesheet">
+
+	<link href="includes/content/content/content1.css" rel="stylesheet">
+
+	<link href="includes/content/galleries/gallery1.css" rel="stylesheet">
+
+	<link href="includes/content/forms/form1.css" rel="stylesheet">
+
+	<link href="includes/content/contact/contact1.css" rel="stylesheet">
+
+	<link href="includes/content/footers/footer1.css" rel="stylesheet">
 
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <html>
-<head>
-<title></title>
-<?php include "includes/coreincludes.php";?>
-</head>
-<body>
-<?php include ("includes/content/headers/header1.php");?>
-<?php include("includes/footer.php");?>
-</body>
+	<head>
+		<title></title>
+		<?php include "includes/coreincludes.php";?>
+	</head>
+	<body>
+
+		<?php include ("includes/content/topheaders/topheader1.php");?>
+		<?php include ("includes/content/headers/header1.php");?>
+		<?php include ("includes/content/content/content1.php");?>
+		<?php include ("includes/content/galleries/gallery1.php");?>
+		<?php include ("includes/content/forms/form1.php");?>
+		<?php include ("includes/content/contact/contact1.php");?>
+		<?php include("includes/content/footers/footer1.php");?>
+
+	</body>
 </html>
